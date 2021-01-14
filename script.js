2503da835254837feca5793f48aab91da25665ca
Jon Williams <jrwilliams100@hotmail.co.uk>
2021-01-14 15:05:17 +0000

    Added first working regular expression to check for lowercase letter in JS

--- script.js
+++ script.js
@@ -4,22 +4,31 @@ const errorElement = document.getElementById('error');
 
 
 form.addEventListener('submit', (e) => {
-  let messages = []
+    let messages = []
 
-  if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
-  }
+    if (password.value.length <= 6) {
+        messages.push('Password must be longer than 6 characters')
+    }
 
-  if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
-  }
+    if (password.value.length >= 20) {
+        messages.push('Password must be less than 20 characters')
+    }
 
-  if (password.value === 'password') {
-    messages.push('Password cannot be password')
-  }
+    let lowerCaseLetters = /[a-z]/g;
 
-  if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-  }
+    if (!password.value.match(lowerCaseLetters)) {
+        messages.push('Password needs to contain at least one lowercase letter')
+    }
+
+   
+
+    if (password.value === 'password') {
+        messages.push('Password cannot be password')
+    }
+
+    // If there is any error message, the form will not be submitted
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
 })
